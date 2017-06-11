db81514c0715ae81f4161f49efef406181c2b902
José Ignacio Guerrero VInueza <joseguerrerov@users.noreply.github.com>
2017-06-11 12:18:16 -0500

    initial upload

---
+++ src/Components/Load.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+
+class Load extends Component{
+  render(){
+    return(
+      <div className="round1">
+        <div className="round2">
+          <div className="round3"></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Load
